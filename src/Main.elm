69b9900d06434e2facb851f1d5a1e7024d81ccee
Diego Nieto Cid <dnietoc@gmail.com>
2016-12-18 23:39:52 -0300

    Implement a static view of the root of the tree

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,187 @@
 module Main exposing (main)
 
-import Html exposing (Html)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.App as Html
+import Result.Extra as Result
+import Hierarchy as H exposing (Hierarchy)
+import Data.Api.Endpoints as Api
 
 
--- Import Modules
+buildInitialHierarchy : Result String Hierarchy
+buildInitialHierarchy =
+    let
+        folders =
+            Api.audience_folders
 
-import Data.Audience
-import Data.AudienceFolder
+        audiences =
+            Api.audiences
+
+        buildHierarchy folders audiences =
+            let
+                withFolders =
+                    List.foldl
+                        (\a b -> b `Result.andThen` (H.registerFolder a))
+                        (Ok H.empty)
+                        folders
+            in
+                List.foldl
+                    (\a b -> b `Result.andThen` (H.registerFile a))
+                    withFolders
+                    audiences
+    in
+        folders `Result.andThen` \a -> audiences `Result.andThen` \b -> buildHierarchy a b
+
+
+type Model
+    = Error String
+    | Loaded (Maybe H.Node) Hierarchy
+
+
+init : Model
+init =
+    Result.unpack Error (Loaded Nothing) buildInitialHierarchy
+
+
+view : Model -> Html msg
+view model =
+    case model of
+        Error s ->
+            Html.text s
+
+        Loaded selected hierarchy ->
+            viewHierarchyLayout selected hierarchy
+
+
+viewHierarchyLayout : Maybe H.Node -> Hierarchy -> Html msg
+viewHierarchyLayout selected hierachy =
+    div
+        [ class "container-fluid" ]
+        [ div [ class "row" ]
+            [ div [ class "col-xs-3" ]
+                [ viewHierarchySideBar hierachy ]
+            , div [ class "col-xs-9" ]
+                [ viewHierarchyInfo selected hierachy ]
+            ]
+        ]
+
+
+viewHierarchySideBar : Hierarchy -> Html msg
+viewHierarchySideBar h =
+    div [ class "row" ]
+        [ div [ class "col-xs-12" ]
+            (viewCurrentNodes h)
+        ]
+
+
+viewCurrentNodes : Hierarchy -> List (Html msg)
+viewCurrentNodes h =
+    let
+        res =
+            H.currentNodes h
+
+        hasUp =
+            h
+                |> H.currentPath
+                |> Result.map List.length
+                |> Result.map ((>) 0)
+                |> Result.withDefault False
+    in
+        case res of
+            Ok nodes ->
+                let
+                    upEntry =
+                        if hasUp then
+                            [ viewUp ]
+                        else
+                            []
+                in
+                    List.append upEntry <| List.map viewNode nodes
+
+            Err msg ->
+                [ div [ class "alert alert-error" ] [ text msg ] ]
+
+
+viewUp : Html msg
+viewUp =
+    viewFolderEntry "Go Up" "go-up"
+
+
+viewNode : H.Node -> Html msg
+viewNode node =
+    case node of
+        H.File f ->
+            a [ href "#", class "audience" ]
+                [ div [ class "panel panel-default" ]
+                    [ text f.name ]
+                ]
+
+        H.Folder count f ->
+            viewFolderEntry
+                (f.name ++ " (" ++ (toString count) ++ ")")
+                ""
+
+
+viewFolderEntry : String -> String -> Html msg
+viewFolderEntry label extraClasses =
+    a [ href "#", class ("folder " ++ extraClasses) ]
+        [ div [ class "panel panel-default" ]
+            [ span
+                [ class "glyphicon glyphicon-folder-close" ]
+                []
+            , text label
+            ]
+        ]
+
+
+viewHierarchyInfo : Maybe H.Node -> Hierarchy -> Html msg
+viewHierarchyInfo s h =
+    div [ class "row" ]
+        [ div [ class "col-xs-12" ]
+            [ viewBreadcrumb h ]
+        , div [ class "col-xs-12" ]
+            [ pre [] [ text <| toString s ] ]
+        ]
+
+
+viewBreadcrumb : Hierarchy -> Html msg
+viewBreadcrumb h =
+    let
+        history =
+            H.currentPath h
+    in
+        case history of
+            Ok path ->
+                ol [ class "breadcrumb" ]
+                    ((li [] [ a [ href "#" ] [ text "Root" ] ])
+                        :: (List.map viewBreadcrumbItem path)
+                    )
+
+            Err s ->
+                div [ class "alert alert-info", attribute "role" "alert" ]
+                    [ text s ]
+
+
+viewBreadcrumbItem : H.Node -> Html msg
+viewBreadcrumbItem node =
+    let
+        name =
+            case node of
+                H.File f ->
+                    f.name
+
+                H.Folder _ f ->
+                    f.name
+    in
+        li [] [ a [ href "#" ] [ text name ] ]
 
 
 {-| Main file of application
 -}
-main : Html msg
+main : Program Never
 main =
-    Html.text "There will be app soon!"
+    Html.beginnerProgram
+        { model = init
+        , view = view
+        , update = identity
+        }
