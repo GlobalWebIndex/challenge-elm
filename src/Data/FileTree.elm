9bb5f706ba9cf30a799f2cb3417a3f2c1d455c83
mathiajusth <matus.ledenyi@gmail.com>
2019-05-17 14:45:39 +0200

    adding FileTreeFocused data type

--- src/Data/FileTree.elm
+++ src/Data/FileTree.elm
@@ -1,17 +1,24 @@
-module Data.FileTree exposing (FileTree(..), FolderId, FolderName, filterFiles, mapFolders, reverse, sortWith, toList)
+module Data.FileTree exposing (FileTree(..), FolderName, filterFiles, mapFolders, reverse, sortWith, toList)
+
+---------------------
+-- FileTree data type
+---------------------
+
+import Data.ListFocused exposing (ListWithHole(..), focusIn)
+import List.Extra exposing (uncons)
 
 
 type alias FolderName =
     String
 
 
-type alias FolderId =
-    Int
-
-
 type FileTree file
     = File file
-    | Folder FolderName (List (FileTree file))
+    | Folder FolderName (FileForest file)
+
+
+type alias FileForest a =
+    List (FileTree a)
 
 
 toList : FileTree file -> List file
@@ -66,3 +73,56 @@ reverse =
 sortWith : (FileTree a -> FileTree a -> Order) -> FileTree a -> FileTree a
 sortWith sorter =
     mapFolders (List.sortWith sorter)
+
+
+
+-----------------------------
+-- Focused FileTree data type
+-----------------------------
+
+
+type FolderWithHole a
+    = FolderWithHole FolderName (FileForest a) (FileForest a)
+
+
+type alias FileTreeFocused a =
+    ( List (FolderWithHole a), FileTree a )
+
+
+focus : FileTree a -> Maybe (FileTreeFocused a)
+focus tree =
+    case tree of
+        File _ ->
+            Nothing
+
+        Folder _ _ ->
+            Just ( [], tree )
+
+
+stepUp : FileTreeFocused a -> Maybe (FileTreeFocused a)
+stepUp focTree =
+    case focTree of
+        ( [], _ ) ->
+            Nothing
+
+        ( (FolderWithHole name leftOfHole rightOfHole) :: crumbs, tree ) ->
+            Just ( crumbs, Folder name <| leftOfHole ++ [ tree ] ++ rightOfHole )
+
+
+stepDown : Int -> FileTreeFocused a -> Maybe (FileTreeFocused a)
+stepDown n ( crumbs, tree ) =
+    case tree of
+        File _ ->
+            Nothing
+
+        Folder name forest ->
+            let
+                focusedForest =
+                    focusIn n forest
+            in
+            case focusedForest of
+                Nothing ->
+                    Nothing
+
+                Just ( ListWithHole left right, focusedTree ) ->
+                    Just ( FolderWithHole name left right :: crumbs, focusedTree )
