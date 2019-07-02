e4d1ce745d40b8cb53e629087088d01676749b42
Anton Ovechkin <owanturist@gmail.com>
2019-07-02 18:30:36 +0700

    Add extracting public functions for Store

--- src/Data/Store.elm
+++ src/Data/Store.elm
@@ -1,4 +1,12 @@
-module Data.Store exposing (AudienceFolderID, AudienceID, Store)
+module Data.Store exposing
+    ( AudienceFolderID
+    , AudienceID
+    , AudienceLevel
+    , Store
+    , getLevelByFolderID
+    , getRootLevel
+    , init
+    )
 
 import Data.Audience exposing (Audience)
 import Data.AudienceFolder exposing (AudienceFolder)
@@ -15,31 +23,37 @@ type alias AudienceID =
 
 
 type alias AudienceLevel =
+    { folders : List AudienceFolder
+    , audiences : List Audience
+    }
+
+
+type alias Level =
     { folders : Set AudienceFolderID
     , audiences : Set AudienceID
     }
 
 
-emptyLevel : AudienceLevel
+emptyLevel : Level
 emptyLevel =
-    AudienceLevel Set.empty Set.empty
+    Level Set.empty Set.empty
 
 
-insertFolderToLevel : AudienceFolderID -> AudienceLevel -> AudienceLevel
+insertFolderToLevel : AudienceFolderID -> Level -> Level
 insertFolderToLevel folderID level =
     { level | folders = Set.insert folderID level.folders }
 
 
-insertAudienceToLevel : AudienceID -> AudienceLevel -> AudienceLevel
+insertAudienceToLevel : AudienceID -> Level -> Level
 insertAudienceToLevel audienceID level =
     { level | audiences = Set.insert audienceID level.audiences }
 
 
 type alias Levels =
-    Dict AudienceFolderID AudienceLevel
+    Dict AudienceFolderID Level
 
 
-updateOnLevels : AudienceFolderID -> (AudienceLevel -> AudienceLevel) -> Levels -> Levels
+updateOnLevels : AudienceFolderID -> (Level -> Level) -> Levels -> Levels
 updateOnLevels folderID tagger levels =
     Dict.update folderID (Just << tagger << Maybe.withDefault emptyLevel) levels
 
@@ -49,7 +63,7 @@ type Store
         { folders : Dict AudienceFolderID AudienceFolder
         , audiences : Dict AudienceID Audience
         }
-        { root : AudienceLevel
+        { root : Level
         , levels : Levels
         }
 
@@ -100,18 +114,43 @@ init listOfFolders listOfAudiences =
     Store data2 relations2
 
 
+extractListFromData : Set comparable -> Dict comparable { a | id : comparable } -> List { a | id : comparable }
+extractListFromData ids entities =
+    Set.foldr
+        (\id acc ->
+            case Dict.get id entities of
+                Nothing ->
+                    acc
 
--- getLevelByFolderID : Maybe AudienceFolderID -> Store -> AudienceLevel
--- getLevelByFolderID targetFolderID (Store { folders, audiences }) =
---     { folders =
---         folders
---             |> Dict.filter (\_ folder -> targetFolderID == folder.parent)
---             |> Dict.values
---     , audiences =
---         audiences
---             |> Dict.filter (\_ audience -> targetFolderID == audience.folder)
---             |> Dict.values
---     }
--- getRoot : Store -> AudienceLevel
--- getRoot =
---     getLevelByFolderID Nothing
+                Just entity ->
+                    entity :: acc
+        )
+        []
+        ids
+
+
+extractLevelByParentID : Maybe AudienceFolderID -> Store -> Maybe AudienceLevel
+extractLevelByParentID mFolderID (Store data { root, levels }) =
+    Maybe.map
+        (\{ folders, audiences } ->
+            { folders = extractListFromData folders data.folders
+            , audiences = extractListFromData audiences data.audiences
+            }
+        )
+        (case mFolderID of
+            Nothing ->
+                Just root
+
+            Just folderID ->
+                Dict.get folderID levels
+        )
+
+
+getRootLevel : Store -> Maybe AudienceLevel
+getRootLevel =
+    extractLevelByParentID Nothing
+
+
+getLevelByFolderID : AudienceFolderID -> Store -> Maybe AudienceLevel
+getLevelByFolderID =
+    extractLevelByParentID << Just
